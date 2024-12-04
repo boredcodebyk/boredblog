077abb471d9fa7ce294b53bd11aedead47d6edbb
K <25322315+boredcodebyk@users.noreply.github.com>
2024-12-04 18:07:27 +0530

    fix overflow

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,15 +4,15 @@
 </script>
 
 <div
-  class="bg-emerald-100 h-dvh selection:text-emerald-100 selection:bg-gray-700"
+  class="bg-emerald-100 h-dvh selection:text-emerald-100 selection:bg-gray-700 sm:px-20 lg:px-60 px-10 py-10 flex flex-col overflow-auto"
 >
-  <div class="sm:px-20 lg:px-60 px-10 py-10">
+  
     <p>boredcodebyk // illusbyk // cloudy echoes</p>
     <div class="flex flex-row gap-2 py-2">
       <a href="/">Home</a>
       <a href="/about">About</a>
       <a href="https://github.com/boredcodebyk" target="_blank">GitHub</a>
-    </div>
+    </div>  
     {@render children()}
-  </div>
+  
 </div>
