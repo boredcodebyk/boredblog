388b33097ce3700a5dec1869e7873b179bf80f96
K <25322315+boredcodebyk@users.noreply.github.com>
2025-04-17 20:03:00 +0530

    update post list layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,16 +10,20 @@
 
 <h1>Hello There</h1>
 
-<p>my blog(s)</p>
-<div class="py-2">
-  {#if data.posts.length != 0}
-    {#each data.posts as post}
-      <p>
-        [{moment(post.date).format("MMMM Do YYYY")}] |
-        <a href={post.slug}>{post.title}</a>
-      </p>
-    {/each}
-  {:else}
-    <p>Come back later</p>
-  {/if}
-</div>
+<p class="py-2">my blog(s)</p>
+
+{#if data.posts.length != 0}
+  <table class="py-2 table-auto">
+    <tbody>
+      {#each data.posts as post}
+        <tr>
+          <td> [{moment(post.date).format("MMMM Do YYYY")}] </td>
+          <td> | </td>
+          <td> <a href={post.slug}>{post.title}</a> </td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+{:else}
+  <p>Come back later</p>
+{/if}
